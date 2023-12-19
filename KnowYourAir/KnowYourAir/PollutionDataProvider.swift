ada6e9e3e4e3267d990bb718f0ced2cc7f16d9c0
ROWAN WANG <rowanwang@192.168.1.216>
2023-12-19 13:43:48 +0000

    update

--- KnowYourAir/KnowYourAir/PollutionDataProvider.swift
+++ KnowYourAir/KnowYourAir/PollutionDataProvider.swift
@@ -8,9 +8,15 @@
 import Foundation
 
 class PollutionDataProvider {
-    func fetchPollutionData() async -> AirQuality {
+    func fetchPollutionData() async throws -> AirQuality {
+        //create the new url
+        let url = URL(string: "http://api.openweathermap.org/data/2.5/air_pollution?lat=51.5&lon=0.13&appid=27b3233fb194145bebbf28f441970661".addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? "")
+        //create a new urlRequest passing the url
+        let request = URLRequest(url: url!)
+        //run the request and retrieve both the data and the response of the call
+        let (data, response) = try await URLSession.shared.data(for: request)
         
-    return AirQuality()
+        return AirQuality()
     }
 }
 
